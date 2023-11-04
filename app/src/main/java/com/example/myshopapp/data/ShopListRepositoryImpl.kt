5a7bf862b1ecc975da468f92b468889302dc6a2d
YuraSopa <aspiresopa@gmail.com>
2023-11-04 19:26:46 +0200

    Created presentation Added ViewModel Added MutableLivedata

--- app/src/main/java/com/example/myshopapp/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/myshopapp/data/ShopListRepositoryImpl.kt
@@ -1,23 +1,35 @@
 package com.example.myshopapp.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.myshopapp.domain.ShopItem
 import com.example.myshopapp.domain.ShopListRepository
 import java.lang.RuntimeException
 
 object ShopListRepositoryImpl : ShopListRepository {
 
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
     private var autoIncrement = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("Name$i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrement++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -32,7 +44,11 @@ object ShopListRepositoryImpl : ShopListRepository {
         } ?: throw RuntimeException("Element with id: $shopItemId not found!")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 }
