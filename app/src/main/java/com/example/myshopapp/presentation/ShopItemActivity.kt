df63c2dd1064a23ff0c789e7fc8093ace7097258
YuraSopa <aspiresopa@gmail.com>
2023-11-06 21:56:38 +0200

    Created ShopItemActivity

--- app/src/main/java/com/example/myshopapp/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/myshopapp/presentation/ShopItemActivity.kt
@@ -1,16 +1,150 @@
 package com.example.myshopapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.myshopapp.R
+import com.example.myshopapp.domain.ShopItem
+import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
 
+    private lateinit var textInputLayoutName: TextInputLayout
+    private lateinit var textInputLayoutCount: TextInputLayout
+    private lateinit var editTextName: EditText
+    private lateinit var editTextCount: EditText
+    private lateinit var buttonSave: Button
+
     private lateinit var viewModel: ShopItemViewModel
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemID = ShopItem.UNDEFINED_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
 
+        parseIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews()
+        addTextChangeListeners()
+        launchRightMode()
+        observeViewModel()
 
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputName.observe(this) {
+            if (it == true) {
+                textInputLayoutName.error = getString(R.string.error_invalid_name)
+            } else {
+                textInputLayoutName.error = null
+            }
+        }
+        viewModel.errorInputCount.observe(this) {
+            if (it == true) {
+                textInputLayoutCount.error = getString(R.string.error_invalid_count)
+            } else {
+                textInputLayoutCount.error = null
+            }
+        }
+        viewModel.closeScreen.observe(this) {
+            finish()
+        }
+    }
+
+    private fun launchRightMode() {
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+
+    private fun addTextChangeListeners() {
+        editTextName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        editTextCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputCount()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun launchEditMode() {
+        viewModel.getShopItem(shopItemID)
+        viewModel.shopItem.observe(this) {
+            editTextName.setText(it.name)
+            editTextCount.setText(it.count.toString())
+        }
+        buttonSave.setOnClickListener {
+            viewModel.editShopItem(editTextName.text?.toString(), editTextCount.text?.toString())
+        }
+    }
+
+    private fun launchAddMode() {
+        buttonSave.setOnClickListener {
+            viewModel.addShopItem(editTextName.text?.toString(), editTextCount.text?.toString())
+        }
+    }
+
+    private fun initViews() {
+        textInputLayoutName = findViewById(R.id.textInputLayout_name)
+        textInputLayoutCount = findViewById(R.id.textInputLayout_count)
+        editTextName = findViewById(R.id.editText_name)
+        editTextCount = findViewById(R.id.editText_count)
+        buttonSave = findViewById(R.id.buttonSave)
+    }
+
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent!")
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode: $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Param shopItemID is absent!")
+            }
+            shopItemID = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopItemID: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemID)
+            return intent
+        }
     }
 }
