d4f267ae78f840508df6440ade35f3440db58ec7
YuraSopa <aspiresopa@gmail.com>
2023-11-15 15:45:28 +0200

    Added dataBinding Added BindingAdapter Added Database, Dao Added DbModel, Mapper Also added Coroutine

--- app/src/main/java/com/example/myshopapp/domain/AddShopItemUseCase.kt
+++ app/src/main/java/com/example/myshopapp/domain/AddShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.myshopapp.domain
 
 class AddShopItemUseCase(private val shopListRepository: ShopListRepository) {
-    fun addShopItem(shopItem: ShopItem){
+    suspend fun addShopItem(shopItem: ShopItem){
         shopListRepository.addShopItem(shopItem)
     }
 }
